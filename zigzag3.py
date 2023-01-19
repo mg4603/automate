35a79d2d439431c7ffd641b708fe76591f4d69f3
Michael George <michael.g.george@gmail.com>
2023-01-20 04:45:59 +0530

    Add main to zigzag3 in basic

---
+++ zigzag3.py
@@ -0,0 +1,20 @@
+from time import sleep
+
+def main():
+    indent  = 0
+    indent_increasing = True
+
+    while True:
+        sleep(DELAY)
+        print('{}*******'.format(' ' * indent))
+        sleep()
+        if indent_increasing:
+            indent += 1
+            if indent == 20:
+                indent_increasing = not indent_increasing
+        else:
+            indent -= 1
+            if indent == 0:
+                indent_increasing = not indent_increasing
+
+
