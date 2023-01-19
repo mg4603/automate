7c1d5ecafe651e4a626ec6bb301a3e9c2780e48c
Michael George <michael.g.george@gmail.com>
2023-01-20 02:08:38 +0530

    Add main to rps_game1 in beginner

--- basics/rps_game1.py
+++ basics/rps_game1.py
@@ -25,3 +25,44 @@ def get_word(code):
     elif code == 'S':
         return 'Scissors'
 
+def main():
+    print('ROCK, PAPER, SCISSORS')
+
+    wins = 0
+    loss = 0
+    ties = 0
+
+    while True:
+        print(
+            'Wins: %s, Losses: %s, Ties: %s' 
+                %
+            (wins, loss, ties) 
+        )
+        move = get_move()
+        print('%s versus...' % get_word(move))
+        sleep(1)
+        computer_move = choice(('r', 'p', 's'))
+        print(get_word(computer_move))
+
+        if move == computer_move:
+            ties += 1
+            print('It\'s a tie!')
+        elif move == 'r' and computer_move == 's':
+            wins + 1
+            print('You win!')
+        elif move == 'p' and computer_move == 'r':
+            wins += 1
+            print('You win!')
+        elif move == 's' and computer_move == 'p':
+            wins += 1
+            print('You win!')
+        elif move == 's' and computer_move == 'r':
+            loss += 1
+            print('You lose!')
+        elif move == 'r' and computer_move == 'p':
+            loss += 1
+            print('You lose!')
+        elif move == 'p' and computer_move == 's':
+            loss += 1
+            print('You lose!')
+           
