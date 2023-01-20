22a1dba2799fff3b8bda80e989edd21f5b432199
Michael George <michael.g.george@gmail.com>
2023-01-20 07:27:17 +0530

    Add DELAY to conways_game_of_life4 in basics

--- basics/conways_game_of_life4.py
+++ basics/conways_game_of_life4.py
@@ -1,12 +1,14 @@
 from sys import exit
 from copy import deepcopy
 from time import sleep
+from random import choice
 DEAD = ' '
 ALIVE = '#'
 
 WIDTH = 60
 HEIGHT = 40
 
+DELAY = 1
 
 if __name__ == '__main__':
     try:
