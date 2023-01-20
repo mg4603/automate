0b63e36fac583ab1859502b2ad0074cdce1b6c19
Michael George <michael.g.george@gmail.com>
2023-01-21 00:18:49 +0530

    Define RandomContinueZombie in my_zombie6 in basics

--- basics/my_zombie6.py
+++ basics/my_zombie6.py
@@ -1,3 +1,29 @@
+from sys import exit
+from random import randint
+try:
+    from zombiedice.examples import (
+        RandomCoinFlipZombie, RollsUntilInTheLeadZombie, 
+        MinNumShotgunsThenStopsZombie,
+    )
+    from zombiedice import runWebGui, runTournament, roll
+except ImportError:
+    err_msg = '''This program requires the zombie-dice module to run.
+Installation instructions can be found at https://pypi.org/project/zombie-dice/
+
+'''
+    exit(err_msg)
+
+class RandomContinueZombie:
+    def __init__(s, name):
+        s.name = name
+    
+    def turn(s, game_state):
+        while True:
+            dice_roll_results = roll()
+            if randint(0, 1) == 0:
+                break
+
+
 def main():
     zombies = (
     RandomCoinFlipZombie(name='Random'),
@@ -5,7 +31,7 @@ def main():
     MinNumShotgunsThenStopsZombie(name='Stop at 2 Shotguns', minShotguns=2),
     MinNumShotgunsThenStopsZombie(name='Stop at 1 Shotgun', minShotguns=1),
     # Add any other zombie players here.
-    RandomContinueZombie(name='Random'),
+    RandomContinueZombie(name='Random continue'),
     TwoBrainsThenStopsZombie(name='Stops at 2 brains'),
     TwoShotgunsThenStopsZombie(name='Stops at 2 shotguns - created'),
     FourOneRollsWithStopZombie(name='Rolls 1-4 times but stops at 2 shotguns'),
