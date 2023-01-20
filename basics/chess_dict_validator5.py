c1711e38e30586584fd9bd604d55f4f21d0e5303
Michael George <michael.g.george@gmail.com>
2023-01-20 19:26:09 +0530

    Add main to chess_dict_validator5 in basics

--- basics/chess_dict_validator5.py
+++ basics/chess_dict_validator5.py
@@ -1,2 +1,13 @@
+
+def main():
+    board_dict = {
+        '1h': 'bking', 
+        '6c': 'wqueen',
+        '2g': 'bbishop', 
+        '5h': 'bqueen', 
+        '3e': 'wking'
+    }
+    print(is_valid_chess_board(board_dict))
+
 if __name__ == '__main__':
     main()
