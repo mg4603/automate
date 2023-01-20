2346134654b68f77c3d683d18bad0dcd897735b5
Michael George <michael.g.george@gmail.com>
2023-01-20 23:32:13 +0530

    Add main to table_printer in basics

--- basics/table_printer.py
+++ basics/table_printer.py
@@ -1,2 +1,11 @@
+def main():
+    table_data = [
+        ['apples', 'oranges', 'cherries', 'banana'],
+        ['Alice', 'Bob', 'Carol', 'David'],
+        ['dogs', 'cats', 'moose', 'goose']
+    ]
+    print_table(table_data)
+
+
 if __name__ == '__main__':
     main()
