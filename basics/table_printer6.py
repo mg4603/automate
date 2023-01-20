b6d6b104b2af8cbff27460089ec84a2953f9ab32
Michael George <michael.g.george@gmail.com>
2023-01-20 23:38:51 +0530

    Add print_table to table_printer6 in basics

--- basics/table_printer6.py
+++ basics/table_printer6.py
@@ -1,3 +1,16 @@
+def print_table(table_data):
+    col_lengths = [0] * len(table_data)
+    table_height = len(table_data[0])
+    table_width = len(table_data)
+
+    for i, col in enumerate(table_data):
+        col_lengths[i] = max(map(len, col))
+    
+    for y in range(table_height):
+        for x in range(table_width):
+            print(table_data[x][y].rjust(col_lengths[x]), end=' ')
+        print()
+
 def main():
     table_data = [
         ['apples', 'oranges', 'cherries', 'banana'],
