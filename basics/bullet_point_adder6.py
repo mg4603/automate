3bd9f362110425b540e74c547722098845de4e4e
Michael George <michael.g.george@gmail.com>
2023-01-20 22:10:11 +0530

    Add main to bullet_point_adder6 in basics

--- basics/bullet_point_adder6.py
+++ basics/bullet_point_adder6.py
@@ -1,10 +1,16 @@
 from sys import exit
 try:
-    from pyerclip import copy, paste
+    from pyperclip import copy, paste
 except ImportError:
     exit('This script requires pyperclip.')
 
-
+def main():
+    text = paste()
+    new_text = ''
+    for line in text.split('\n'):
+        new_text += '* ' + line + '\n'
+    print(new_text)
+    copy(new_text)
 
 if __name__ == '__main__':
     main()
