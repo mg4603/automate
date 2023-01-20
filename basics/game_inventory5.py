6d96bb11465602c9f662e6858751f22408da2d6d
Michael George <michael.g.george@gmail.com>
2023-01-20 20:12:17 +0530

    Add display_inventory to game_inventory5 in basics

--- basics/game_inventory5.py
+++ basics/game_inventory5.py
@@ -1,3 +1,11 @@
+def display_inventory(inventory):
+    max_length = max(map(len, inventory.keys()))
+    total = 0
+    print('Inventory:')
+    for key, val in inventory.items():
+        print('{} {}'.format(key.title().ljust(max_length), val))
+        total += val
+    print('Total number of items: {}'.format(total))
 
 def main():
     stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
