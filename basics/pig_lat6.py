a2b9d05ca3930c89048a7a64162189b9b0cfdc94
Michael George <michael.g.george@gmail.com>
2023-01-20 22:16:00 +0530

    Add main to pig_lat6 in basics

--- basics/pig_lat6.py
+++ basics/pig_lat6.py
@@ -1,2 +1,11 @@
+
+def main():
+    print('Enter message to translate to Pig Latin:')
+    msg = input('> ')
+    translated_message = translate_to_pig_latin(msg)
+    print()
+    print('Translated Message:')
+    print(translated_message)
+
 if __name__ == '__main__':
     main()
