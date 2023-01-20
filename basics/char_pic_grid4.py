fdf2dd829a87e4a36bc8312f1fd2ef1ab2bb1824
Michael George <michael.g.george@gmail.com>
2023-01-20 08:42:55 +0530

    Add draw_pic_grid to char_pic_grid4 in basics

--- basics/char_pic_grid4.py
+++ basics/char_pic_grid4.py
@@ -9,3 +9,11 @@ GRID = [
     ['.', 'O', 'O', '.', '.', '.'],
     ['.', '.', '.', '.', '.', '.']
 ]
+
+def draw_pic_grid(grid):
+    width = len(grid)
+    height = len(grid[0])
+    for y in range(height):
+        for x in range(width):
+            print(grid[x][y], end='')
+        print()
