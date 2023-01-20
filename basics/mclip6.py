d46f69138b9fd0326c3308ae9080cd81d522a4b3
Michael George <michael.g.george@gmail.com>
2023-01-20 21:51:33 +0530

    Add main to mclip6 in basics

--- basics/mclip6.py
+++ basics/mclip6.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python3
 # mclip - a multi-clipboard program
+from sys import exit
+try:
+    from pyperclip import copy
+except ImportError:
+    exit('This program requires pyperclip.')
+    
+def main():
+    args = parse_args()
+    key = args['key_phrase']
+    if key in multi_clipboard:
+        try:
+            copy(multi_clipboard[key])
+            print('(Copied {} to clipboard.)'.format(multi_clipboard[key]))
+        except NameError:
+            exit('This program requires pyperclip')
 
 if __name__ == '__main__':
     main()
