fce8776dc78b53041f49dad99d3d3191a973b979
Michael George <michael.g.george@gmail.com>
2023-01-21 20:13:35 +0530

    Add runner code to main in regexes

---
+++ regexes/strip.py
@@ -0,0 +1,9 @@
+'''
+1) accept a string input
+2) remove second argument from first arg
+3) if second argument doesn't exist remove whitespaces at start and end
+'''
+
+
+if __name__ == '__main__':
+    main()
