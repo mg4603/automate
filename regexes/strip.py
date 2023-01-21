dedbeac4e44b2f05d56db3eb4449973d478bfd00
Michael George <michael.g.george@gmail.com>
2023-01-21 20:16:09 +0530

    Add main to strip in regexes

--- regexes/strip.py
+++ regexes/strip.py
@@ -4,6 +4,20 @@
 3) if second argument doesn't exist remove whitespaces at start and end
 '''
 
+def main():
+    print('STRIP')
+    print()
+    print('Enter char to strip:')
+    char = input('> ')
+    print('Enter string to strip from:')
+    string = input('> ')
+    if char == '':
+        stripped_string = strip(string)
+    else:
+        stripped_string = strip(string, char)
+    print()
+    print('Stripped string:')
+    print(stripped_string)
 
 if __name__ == '__main__':
     main()
