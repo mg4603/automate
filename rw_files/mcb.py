b2ca3e0385ce1783d5cd0628d84c34a6eee4b0f1
Michael George <michael.g.george@gmail.com>
2023-01-22 04:27:34 +0530

    Add main to mcb in rw_files

--- rw_files/mcb.py
+++ rw_files/mcb.py
@@ -4,7 +4,32 @@
     * list - copies all keywords to clipboard
     * <keyword> - copies val to clipboard
 '''
+from sys import exit
+try:
+    from pyperclip import copy, paste
+except ImportError:
+    exit('This program requires pyperclip.')
+from shelve import open
 
+
+def main():
+    args = parse_args()
+
+    mcb = open('mcb')
+    
+    if args['option'] == 'save':
+        mcb[args['keyword']] = paste()
+    elif args['options'] == 'list':
+        print('\n'.join(mcb.keys()))
+    elif args['option'] == '':
+        copy(mcb[args['keyword']])
+    elif args['option'] == 'delete':
+        if args['keyword'] != '':
+            mcb.pop(args['keyword'])
+        else:
+            mcb.clear()
+    
+    mcb.close()
 
 if __name__ == '__main__':
     main()
