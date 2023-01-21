87668e286820afbe31d2240a390514ea06d8ef98
Michael George <michael.g.george@gmail.com>
2023-01-22 04:14:20 +0530

    Add runner code to mcb in rw_files

---
+++ rw_files/mcb.py
@@ -0,0 +1,10 @@
+'''
+1) parses command line args
+    * save <keyword>- saves the clipboard contents to mcb under keyword
+    * list - copies all keywords to clipboard
+    * <keyword> - copies val to clipboard
+'''
+
+
+if __name__ == '__main__':
+    main()
