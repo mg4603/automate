d3168867d788f90a382674ba1698d078128ee8e8
Michael George <michael.g.george@gmail.com>
2023-01-22 03:27:59 +0530

    Add main to random_quiz_gen in rw_files

--- rw_files/random_quiz_gen.py
+++ rw_files/random_quiz_gen.py
@@ -5,6 +5,28 @@
 4) write quizzes to 35 text files
 5) write answer keys to 35 text files.
 '''
+from sys import exit
+try:
+    from pyinputplus import inputNum
+except ImportError:
+    exit('This program requires pyinputplus')
+from pathlib import Path
+
+def main():
+    cwd = Path('.')
+    output_dir = cwd / 'quizzes'
+    output_dir.mkdir(parents=True)
+
+    print('Enter number of quizzes')
+    num_of_quizzes = inputNum(min=1, prompt='> ')
+    for i in range(num_of_quizzes):
+        quiz, answers = get_quiz(qa_dict)
+        quiz_file = output_dir / ('quiz_{}.txt'.format(i + 1))
+        answer_file = output_dir / ('answer_{}.txt'.format(i + 1))
+        with quiz_file.open('w') as file:
+            file.write(quiz)
+        with answer_file.open('w') as file:
+            file.write(answers)
 
 
 if __name__ == '__main__':
