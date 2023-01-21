2f9e4db7130716d372368e780533eb58b3faca23
Michael George <michael.g.george@gmail.com>
2023-01-21 07:22:13 +0530

    Ad main to date_detector in regexes

--- regexes/date_detector.py
+++ regexes/date_detector.py
@@ -1,3 +1,27 @@
+'''
+1) Copy date from clipboard
+2) create regex to detect date in dd/mm/yyyy format
+3) add function to validate if date exists
+4) paste valid dates to clipboard
+'''
+try:
+    from pyperclip import copy, paste
+except ImportError:
+    exit('This program requires pyperclip.')
+
+def main():
+    text = paste()
+
+    dates = date_regex.findall(text)
+    valid_dates = get_valid_dates(dates)
+    formatted_str = get_formatted_dt_str(valid_dates)
+
+
+    print(formatted_str)
+    copy(formatted_str)
+    print('(Copied to clipboard.')
+
+
 
 if __name__ == '__main__':
     main()
