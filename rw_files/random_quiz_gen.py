a0119be38c0caf74e0d4d715c6c5feb92926193f
Michael George <michael.g.george@gmail.com>
2023-01-22 03:19:25 +0530

    Add runner code to random_quiz_gen in rw_files

---
+++ rw_files/random_quiz_gen.py
@@ -0,0 +1,11 @@
+'''
+1) create 35 quizzes
+2) create 50 mcq for each quiz, in random order
+3) provide the correct answer and 3 wrong answers
+4) write quizzes to 35 text files
+5) write answer keys to 35 text files.
+'''
+
+
+if __name__ == '__main__':
+    main()
