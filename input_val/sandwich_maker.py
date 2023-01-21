ffe2da683e0dd58528d8194bf5a086b81ca75a41
Michael George <michael.g.george@gmail.com>
2023-01-21 23:17:43 +0530

    Add main to sandwich_maker in input_val

--- input_val/sandwich_maker.py
+++ input_val/sandwich_maker.py
@@ -1,8 +1,55 @@
 from sys import exit
 try:
-    from pyinputplus import inputNum, inputChoice, inputMenu
+    from pyinputplus import inputNum, inputChoice, inputMenu, inputYesNo
 except ImportError:
     exit('This program requires pyinputplus.')
+
+
+
+def main():
+    print('Sandwich Maker')
+
+    cost = 0
+    
+    bread = inputMenu(
+        ['wheat', 'white', 'sourdough'],
+        prompt='Choose your bread type:\n'
+    )
+    cost += get_bread_cost(bread)
+    print()
+
+    filling = inputMenu(
+        ['chicken', 'turkey', 'ham', 'tofu'],
+        prompt='Choose your filling:\n'
+    )
+    cost += get_filling_cost(filling)
+    print()
+
+    has_cheese = inputYesNo('Do you want cheese in your sandwich?\n')
+    print()
+
+    if has_cheese == 'yes':
+        cheese = inputMenu(
+            ['cheddar', 'swiss', 'mozzarella'],
+            prompt='Choose your type of cheese:\n'
+            )
+        cost += get_cheese_cost(cheese)
+        print()
+
+    has_mayo = inputYesNo('Do you want mayo in your sandwich?\n')
+    print()
+    has_mustard = inputYesNo('Do you want mustard in your sandwich?\n')
+    print()
+    has_lettuce = inputYesNo('Do you want lettuce in your sandwich?\n')
+    print()
+    has_tomato = inputYesNo('Do you want tomato in your sandwich?\n')
+
+    num_sandwiches = inputNum('How many sandwiches do you want?\n')
+    print()
+
+    print('Total cost of order:')
+    print(cost * num_sandwiches)
+
 
 if __name__ == '__main__':
     main()
