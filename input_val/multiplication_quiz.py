aba737284bd1f14a7296a6e3ba13c1a8d4e82c4a
Michael George <michael.g.george@gmail.com>
2023-01-21 22:50:18 +0530

    Add main to multiplication_quiz in input_val

--- input_val/multiplication_quiz.py
+++ input_val/multiplication_quiz.py
@@ -9,10 +9,35 @@ multiplication quiz
 6) set tries limit to 3
 '''
 from sys import exit
+from random import randint
+from time import sleep
 try:
-    from pyinputplus import inputNum
+    from pyinputplus import inputNum, RetryLimitException, TimeoutException
 except ImportError:
     exit('This program requires pyinpuplus to run.')
+
+def main():
+    score = 0
+    num_of_questions = 10
+    for i in range(num_of_questions):
+        num1 = randint(1, 9)
+        num2 = randint(1, 9)
+        try:
+            prompt = '#{}: {} x {}'.format(i + 1, num1, num2)
+            inputNum(
+                prompt, 
+                allowRegexes='^{}$'.format(num1 * num2),
+                blockRegexes=('.*', 'Incorrect!')
+            )
+        except RetryLimitException:
+            print('Out of tries!')
+        except TimeoutException:
+            print('Out of time!')
+        else:
+            print('Correct!')
+            score += 1
+        sleep(DELAY)
+    print('Score: {} / {}'.format(score, num_of_questions))
 
 if __name__ == '__main__':
     try:
