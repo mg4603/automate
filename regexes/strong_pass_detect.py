a252c334bbf24dcc6c8f8170677567a5a920e6a9
Michael George <michael.g.george@gmail.com>
2023-01-21 19:53:04 +0530

    Add main to strong_pass_detect in regexes

--- regexes/strong_pass_detect.py
+++ regexes/strong_pass_detect.py
@@ -1,3 +1,31 @@
+'''
+requirements:
+1) 8 chars long
+2) has atleast one digit
+3) contains both uppercase and lower case characters
+
+steps:
+1) take password as input
+2) check if requirements are met
+3) return true or false accordingly
+
+'''
+def main():
+    regexes = [length_regex, digit_regex, upper_regex, lower_regex]
+    print('Strong Password')
+    print()
+    print('Requirements:')
+    print('\t1) 8 chars long')
+    print('\t2) Has at least one digit')
+    print('\t3) contains both upper and lower case characters')
+    print()
+    print('Enter password to check:')
+
+    password = input('> ')
+    if is_strong(password, regexes):
+        print('The password matches the required strength conditions.')
+    else:
+        print('The password doesn\'t match the required strength conditions.')
 
 if __name__ == '__main__':
     main()
