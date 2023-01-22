887a28781a82f53ebe341fb8c91ed75721b41566
Michael George <michael.g.george@gmail.com>
2023-01-22 22:15:31 +0530

    Add main to rename_dates in organizing_files

--- organizing_files/rename_dates.py
+++ organizing_files/rename_dates.py
@@ -1,3 +1,14 @@
+'''
+1) get files in user defined dir
+2) Id files with names in european date style
+3) rename files to american date style
+'''
+
+def main():
+    print('Rename Date files')
+    print()
+    dir_path = get_dir_path()
+    rename_files(dir_path, american_date_regex)
 
 if __name__ == '__main__':
     main()
