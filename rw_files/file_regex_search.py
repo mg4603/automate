1f42d27ca83de3a9ab0c0031164d69eb5a418715
Michael George <michael.g.george@gmail.com>
2023-01-22 06:42:28 +0530

    Add main to file_regex_search in rw_files

--- rw_files/file_regex_search.py
+++ rw_files/file_regex_search.py
@@ -4,6 +4,27 @@
 3) check all files for regex
 4) prints results to screen
 '''
+from pathlib import Path
+from re import escape, compile
+
+def main():
+    print('Interactive Grep')
+    print()
+    directory = get_directory()
+    
+    print('Enter regex to search for:')
+    regex_string = input('> ')
+    regex_string = escape(regex_string)
+    regex = compile(regex_string)
+
+    for file in directory.glob('*.txt'):
+        with file.open('r') as f:
+            for line in f.readlines():
+                if regex.search(line):
+                    print('{}: {}'.format(
+                        file.name, line
+                    ))
+                
 
 if __name__ == '__main__':
     main()
