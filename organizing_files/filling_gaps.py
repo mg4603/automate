c32a4cef39abf0968996ceb0fef19929904943ad
Michael George <michael.g.george@gmail.com>
2023-01-23 00:58:21 +0530

    Add get_src_dir to filling_gaps in organizing_files

--- organizing_files/filling_gaps.py
+++ organizing_files/filling_gaps.py
@@ -4,6 +4,19 @@
 3) find missing numbers
 '''
 
+from pathlib import Path
+
+def get_src_dir():
+    print('Enter directory to search:')
+    src_dir_name = input('> ')
+    src_dir_path = Path(src_dir_name).absolute()
+    if src_dir_path.exist():
+        if src_dir_path.is_dir():
+            return src_dir_path
+        print('{} isn\'t a directory.'.format(src_dir_path.name))
+    else:
+        print('{} doesn\'t exist.'.format(src_dir_path.name))
+
 def main():
     print('Filling in the Gaps')
     print()
