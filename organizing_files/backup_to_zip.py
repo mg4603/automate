f9ce693d3adbe59e075c1ceb75185f42fd028bb2
Michael George <michael.g.george@gmail.com>
2023-01-22 23:34:14 +0530

    Add create_backup to backup_to_zip in organizing_files

--- organizing_files/backup_to_zip.py
+++ organizing_files/backup_to_zip.py
@@ -6,6 +6,9 @@
 5) create next numbered backup
 '''
 from pathlib import Path
+from zipfile import ZipFile, ZIP_DEFLATED
+from os import walk
+from os.path import basename, join
 
 def get_zip_src():
     print('Enter file or directory to backup:')
@@ -15,15 +18,35 @@ def get_zip_src():
             return Path(name)
         print('No such file or directory exists.')
 
+def create_backup(path, destination_path):
+    file_to_backup = str(path.absolute().name)
+    i = 1
+    while True:
+        zip_file_name = '{}_{}.zip'.format(file_to_backup, i)
+        zip_file_path = destination_path / zip_file_name
+        if not zip_file_path.exists():
+            break
+        i += 1
+    
+    print('Creating new zip file {}'.format(str(zip_file_path.name)))
+
+    with ZipFile(zip_file_path, 'w') as f:
+        for folder_name, _, file_names in walk(path.absolute()):
+            f.write(folder_name)
+            for file_name in file_names:
+                new_base = basename(folder_name) + '_'
+                if file_name.startswith(new_base) and file_name.endswith('.zip'):
+                    continue
+                f.write(join(folder_name, file_name))
+
 def main():
     print('Backup to Zip')
     print()
     path = get_zip_src()
     print('Enter directory to write backup to:')
     destination_name = input('> ')
-    destination_path = Path(
-        destination_name
-    ).mkdir(
+    destination_path = Path(destination_name)
+    destination_path.mkdir(
         parents=True, exist_ok=True
     )
     create_backup(path, destination_path)
