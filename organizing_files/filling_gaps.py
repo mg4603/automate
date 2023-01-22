b0640ac29edecf1cf74c03f5362bc5ee17994903
Michael George <michael.g.george@gmail.com>
2023-01-23 00:54:56 +0530

    Add main to filling_gaps in organizing_files

--- organizing_files/filling_gaps.py
+++ organizing_files/filling_gaps.py
@@ -4,6 +4,25 @@
 3) find missing numbers
 '''
 
+def main():
+    print('Filling in the Gaps')
+    print()
+    src_dir_path = get_src_dir()
+
+    print('Enter prefix to search for:')
+    prefix = input('> ')
+
+    print('Starting:')
+    files = src_dir_path.glob('*')
+    files = [file.name for file in files if prefix in file.name]
+    gaps = get_gaps(files)
+    if gaps:
+        print('Missing files:')
+        print('\n'.join(gaps))
+    else:
+        print('No gap files found.')
+
+    print('Done')
 
 if __name__ == '__main__':
     main()
