6aeeba3e0b47b07fdb3bf0a7f5f02f7c34e115fe
Michael George <michael.g.george@gmail.com>
2023-01-23 01:26:34 +0530

    Add get_gaps to filling_gaps in organizing_files

--- organizing_files/filling_gaps.py
+++ organizing_files/filling_gaps.py
@@ -5,17 +5,48 @@
 '''
 
 from pathlib import Path
+from re import sub, compile
 
 def get_src_dir():
     print('Enter directory to search:')
     src_dir_name = input('> ')
     src_dir_path = Path(src_dir_name).absolute()
-    if src_dir_path.exist():
+    if src_dir_path.exists():
         if src_dir_path.is_dir():
             return src_dir_path
         print('{} isn\'t a directory.'.format(src_dir_path.name))
     else:
         print('{} doesn\'t exist.'.format(src_dir_path.name))
+
+def get_gaps(files):
+    regex = compile(r'(\D*)(\d+)(\D*)')
+
+    mo = regex.search(files[0])
+    prefix = mo.group(1)
+    suffix = mo.group(3)
+
+    files = [sub(r'\D*(\d+)\D*', r'\1',  file) for file in files]
+    files = list(map(int, files))
+    files.sort()
+
+    gaps = []
+    idx = files[0]
+    for file in files:
+        if file == idx:
+            idx += 1
+        else:
+            gaps.append(idx)
+            idx += 1
+    
+    digits = len(str(files[-1]))
+
+    return [
+        '{}{}{}'.format(
+            prefix,
+            str(gap).zfill(digits),
+            suffix
+        ) for gap in gaps
+    ]
 
 def main():
     print('Filling in the Gaps')
