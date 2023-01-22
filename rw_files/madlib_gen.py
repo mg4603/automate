3daff8d584f206766a80f87239bb3e05d37e102f
Michael George <michael.g.george@gmail.com>
2023-01-22 05:53:50 +0530

    Add runner code to madlib_gen in rw_files

---
+++ rw_files/madlib_gen.py
@@ -0,0 +1,9 @@
+'''
+1) get input_file name
+2) process input file
+3) query wherever adjective, noun, adverb or verb are found
+4) replace with queried values
+5) print result and write to outfile
+'''
+if __name__ == '__main__':
+    main()
