e294df9bbdea559becb61daea7a7e77b7432bc8a
Michael George <michael.g.george@gmail.com>
2023-01-22 22:46:14 +0530

    Add main to backup_to_zip in organizing_files

--- organizing_files/backup_to_zip.py
+++ organizing_files/backup_to_zip.py
@@ -1,2 +1,25 @@
+'''
+1) get directory/ file to backup
+2) get destination dir
+3) check if backup exists in destination
+4) check number on backup if it exists
+5) create next numbered backup
+'''
+from pathlib import Path
+
+def main():
+    print('Backup to Zip')
+    print()
+    path = get_path_backup()
+    print('Enter directory to write backup to:')
+    destination_name = input('> ')
+    destination_path = Path(
+        destination_name
+    ).mkdir(
+        parents=True, exist_ok=True
+    )
+    create_backup(path, destination_path)
+    print('Backup created.')
+
 if __name__ == '__main__':
     main()
