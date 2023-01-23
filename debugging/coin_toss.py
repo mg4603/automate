1e3737b8a262930483a829d2d5d08bc19b78543a
Michael George <michael.g.george@gmail.com>
2023-01-23 23:18:56 +0530

    Add main to coin_toss in debugging

--- debugging/coin_toss.py
+++ debugging/coin_toss.py
@@ -1,6 +1,21 @@
+from random import randint
 from logging import debug, DEBUG, disable, basicConfig, CRITICAL
 basicConfig(level=DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 # disable(CRITICAL)
+
+def main():
+    print('Guess the coin toss! Enter head or tails:')
+    guess = get_guess()
+    toss = randint(0, 1)
+    if toss == guess:
+        print('You got it!')
+    else:
+        print('Nope! Guess again!')
+        guesss = get_guess()
+        if toss == guess:
+            print('You got it!')
+        else:
+            print('Nope. You are really bad at this game.')
 
 if __name__ == '__main__':
     main()
