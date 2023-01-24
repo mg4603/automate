a06b7ed2c3f6f94b628d34e5cea136dba285b47c
Michael George <michael.g.george@gmail.com>
2023-01-25 01:06:33 +0530

    Add parse_args to maplt in webscraping

--- webscraping/maplt.py
+++ webscraping/maplt.py
@@ -3,7 +3,24 @@
 2) open webbrowser and search for it at https://www.google.com/maps/search/
 '''
 from webbrowser import open
+from argparse import ArgumentParser
+try:
+    from pyperclip import paste
+except ImportError:
+    exit('This program requires pyperclip to run.')
 
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument('--location', required=False)
+
+    args = parser.parse_args()
+    if args.location == None:
+        if paste() == '':
+            exit('Location required')
+
+        return {'location': paste()}
+        
+    return parser.parse_args()
 
 def main():
     print('Google Maps Search')
