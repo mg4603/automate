4eb32cdee4ad1466b45d8c4635bfd3cd17cb3dd0
Michael George <michael.g.george@gmail.com>
2023-01-25 00:58:01 +0530

    Add main to maplt in webscraping

--- webscraping/maplt.py
+++ webscraping/maplt.py
@@ -1,5 +1,16 @@
+'''
+1) Get location to search for from commandline or clipboard
+2) open webbrowser and search for it at https://www.google.com/maps/search/
+'''
 from webbrowser import open
 
+
+def main():
+    print('Google Maps Search')
+    args = parse_args()
+
+    open('https://www.google.com/maps/search/{}'.format(args['location']))
+    print('Done')
 
 if __name__ == '__main__':
     main()
