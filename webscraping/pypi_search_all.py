c7c1f2dcf153b2c83d8fd0c1d3c480e1e5b822ea
Michael George <michael.g.george@gmail.com>
2023-01-25 01:38:36 +0530

    Add parse_args to pypi_search_all in webscraping

--- webscraping/pypi_search_all.py
+++ webscraping/pypi_search_all.py
@@ -3,12 +3,28 @@
 2) search on pypi at https://pypi.org/search/?q=
 3) open all search links
 '''
+import webbrowser as wbrowser
+from argparse import ArgumentParser
+try:
+    from pyperclip import paste
+except ImportError:
+    exit('This program requires pyperclip to run.')
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument('--query', required=False)
+    args = parser.parse_args()
+    if args.query == None:
+        if paste() == '':
+            exit('Search query required.')
+        return {'query': paste()}
+    return args
 
 def main():
     print('Pypi Search All')
     args = parse_args()
 
-    wbrowser.open('https://pypi.org/search/?q={}'.format(args['q']))
+    wbrowser.open('https://pypi.org/search/?q={}'.format(args['query']))
     print('Done')
 
 if __name__ == '__main__':
