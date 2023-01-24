a1bdbdacdb7f09dba42ef7b49e2bdd96a52d243d
Michael George <michael.g.george@gmail.com>
2023-01-25 03:40:12 +0530

    Add main to downloadXkcd in webscraping

---
+++ webscraping/downloadXkcd.py
@@ -0,0 +1,12 @@
+'''
+1) get xkcd homepage
+2) download comic
+3) go to previous page
+4) repeat 2 and 3 till it gets to first page
+5) optionally take a number of pages to download
+'''
+
+
+
+if __name__ == '__main__':
+    main()
