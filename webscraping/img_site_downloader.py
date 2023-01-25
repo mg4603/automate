71283242945ea1df9db60e0f7ee583f74662931a
Michael George <michael.g.george@gmail.com>
2023-01-25 05:30:59 +0530

    Add runner code to img_site_downloader in webscraping

---
+++ webscraping/img_site_downloader.py
@@ -0,0 +1,10 @@
+'''
+1) take category as cmdline arg or from clipboard
+2) take destination folder
+3) search for images
+4) download images that match type
+'''
+
+
+if __name__ == '__main__':
+    main()
