8e9b34a83f315dc5597e2c6d0d3af7b470ff049a
Michael George <michael.g.george@gmail.com>
2023-01-26 00:07:52 +0530

    Add parse_args to link_verification in webscraping

--- webscraping/link_verification.py
+++ webscraping/link_verification.py
@@ -1,3 +1,29 @@
+'''
+1) get url from clipboard or as argument
+2) get links on page
+3) download every downloadable page and print broken links
+'''
+
+from sys import exit
+from argparse import ArgumentParser
+try:
+    from pyperclip import paste
+except ImportError:
+    exit('This program requires the pyperclip module to run.')
+try:
+    from requests import get
+except ImportError:
+    exit('This program requires the requests module to run.')
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument('-u', '--url', required=False)
+    url = parser.parse_args().url
+    if url == None:
+        if paste() == '':
+            exit('Url required, either as argument or saved to clipboard')
+        url = paste()
+    return {'url': url}
 
 if __name__ == '__main__':
     main()
