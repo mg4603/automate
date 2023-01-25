97127d3ea82be4df8a5ac81e82e08cc91bdcad6c
Michael George <michael.g.george@gmail.com>
2023-01-25 20:39:53 +0530

    Add parse_args to img_site_downloader in webscraping

--- webscraping/img_site_downloader.py
+++ webscraping/img_site_downloader.py
@@ -4,7 +4,26 @@
 3) search for images
 4) download images that match type
 '''
+from argparse import ArgumentParser
+from pathlib import Path
+try:
+    from requests import get
+except ImportError:
+    exit('This program requires the requests module.')
+try:
+    from pyperclip import paste
+except ImportError:
+    exit('This program requires the pyperclip module.')
 
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument('--category', required=False)
+    category = parser.parse_args().category
+    if category == None:
+        if paste() == '':
+            exit('No search category given.')
+        category = paste()
+    return {'category': category}
 
 if __name__ == '__main__':
     main()
