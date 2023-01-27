edf30177b1c919ae11a27a2d368b65d4e15e0dc0
Michael George <michael.g.george@gmail.com>
2023-01-27 06:23:58 +0530

    Add parse_args to blank_row_inserter in openpyxl

--- openpyxl/blank_row_inserter.py
+++ openpyxl/blank_row_inserter.py
@@ -1,3 +1,14 @@
+from pathlib import Path
+from argparse import ArgumentParser
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument('N', help='row to start insertions after')
+    parser.add_argument('M', help='number of blank rows to insert')
+    parser.add_argument('file', help='spread sheet to perform insertions on')
+    return parser.parse_args()
+
+
 
 if __name__ == '__main__':
     main()
