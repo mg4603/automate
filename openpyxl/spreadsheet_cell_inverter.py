b6b5f244b1f99ab6d01a670780c3bca776ea4beb
Michael George <michael.g.george@gmail.com>
2023-01-27 09:04:09 +0530

    Add parse_args to spreadsheet_cell_inverter in openpyxl

--- openpyxl/spreadsheet_cell_inverter.py
+++ openpyxl/spreadsheet_cell_inverter.py
@@ -1,3 +1,15 @@
+from argparse import ArgumentParser
+from sys import exit
+from pathlib import Path
+try:
+    from openpyxl import load_workbook
+except ImportError:
+    exit('This program requires the openpyxl module to run.')
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument('file', help='file to invert cells of')
+    return parser.parse_args()
 
 if __name__ == '__main__':
     main()
