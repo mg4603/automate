c95e47b7276603960a15724158381dad0b119e7d
Michael George <michael.g.george@gmail.com>
2023-01-29 09:51:28 +0530

    Add headings to writing_docx.py in pdf_word

--- pdf_word/writing_docx.py
+++ pdf_word/writing_docx.py
@@ -18,5 +18,15 @@ def multiple_paragraphs():
     para_obj1.add_run(' This text is being added to the second paragraph.')
     doc.save('multiple_paragraphs.docx')
 
-multiple_paragraphs()
+def headings():
+    doc = Document()
+    doc.add_heading('Header 0', 0)
+    doc.add_heading('Header 1', 1)
+    doc.add_heading('Header 2', 2)
+    doc.add_heading('Header 3', 3)
+    doc.add_heading('Header 4', 4)
+    doc.save('headings.docx')
+
+# headings()
+# multiple_paragraphs()
 # hello_world()
