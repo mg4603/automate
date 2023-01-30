3db31f443b2009592338a15eb7924fe8cda2d71a
Michael George <michael.g.george@gmail.com>
2023-01-30 08:28:30 +0530

    Add parse_args to pdf_paranoia in pdf_word

--- pdf_word/pdf_paranoia.py
+++ pdf_word/pdf_paranoia.py
@@ -1,3 +1,23 @@
+from pathlib import Path
+from sys import exit
+from argparse import ArgumentParser
+try:
+    from PyPDF2 import PdfFileReader, PdfFileWriter
+except ImportError:
+    exit('This program requires PyPDF2 to run.')
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument(
+        'dir_path', metavar='dir-path', 
+        help='Directory path to files to encrypt or decrypt'
+    )
+    parser.add_argument(
+        '-e', '--encrypt', action='store_true', 
+        help='Encrypt or Decrypt'
+    )
+    return parser.parse_args()
+
 
 if __name__ == '__main__':
     main()
