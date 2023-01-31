fe6cd3f2dee43008c225d9369f3afe19e11feb7b
Michael George <michael.g.george@gmail.com>
2023-01-31 06:51:41 +0530

    Add parse_args to custom_invitations in pdf_word

--- pdf_word/custom_invitations.py
+++ pdf_word/custom_invitations.py
@@ -1,2 +1,21 @@
+from argparse import ArgumentParser
+from sys import exit
+try:
+    from docx import Document
+except ImportError:
+    exit('This program requires python-docx module to run.')
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument(
+        'guest_list', metavar='guest-list',
+        help='plaintext file with list of guests to create invitations for'
+    )
+    parser.add_argument(
+        'invitation_doc', metavar='invitation-doc',
+        help='word document of invitations to write to.'
+    )
+    return parser.parse_args()
+
 if __name__ == '__main__':
     main()
