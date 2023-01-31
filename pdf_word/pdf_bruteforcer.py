eeea82bb94b63b707d4f26c203d76260c80937b3
Michael George <michael.g.george@gmail.com>
2023-02-01 04:35:40 +0530

    Add parse_args to pdf_bruteforcer in pdf_word

--- pdf_word/pdf_bruteforcer.py
+++ pdf_word/pdf_bruteforcer.py
@@ -1,2 +1,20 @@
+from argparse import ArgumentParser
+from pathlib import Path
+from sys import exit
+try:
+    from docx import Document
+except ImportError:
+    exit('This program requires the python-docx module to run.')
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument(
+        'wordlist', help='wordlist of passwords to bruteforce'
+    )
+    parser.add_argument(
+        'pdf', help='encrypted pdf to bruteforce password of'
+    )
+    return parser.parse_args()
+
 if __name__ == '__main__':
     main()
