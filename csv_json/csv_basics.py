cbd88cea0f67be4f7fb0f24b640da132cc5303c0
Michael George <michael.g.george@gmail.com>
2023-02-02 06:32:40 +0530

    Add read_with_for_loop to csv_basics in csv_json

--- csv_json/csv_basics.py
+++ csv_json/csv_basics.py
@@ -7,4 +7,12 @@ def csv_as_list(filename):
         csv_data_as_list = list(csv_reader)
         return csv_data_as_list
 
-print(csv_as_list('example.csv'))
+def read_with_for_loop(filename):
+    with Path(filename).open('r') as file:
+        csv_reader = reader(file)
+        for row in csv_reader:
+            print('Row #{} {}'.format(csv_reader.line_num, row))
+
+
+# read_with_for_loop('example.csv')
+# print(csv_as_list('example.csv'))
