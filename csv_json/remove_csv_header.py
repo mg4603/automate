328fd70a990a017435b8601de0045f5b1aa31e3a
Michael George <michael.g.george@gmail.com>
2023-02-03 01:17:39 +0530

    Add parse_args to remove_csv_header in csv_json

--- csv_json/remove_csv_header.py
+++ csv_json/remove_csv_header.py
@@ -1,3 +1,18 @@
+'''
+1) find all csv files in the cwd
+2) read csv files
+3) remove headers 
+4) write to new folder
+'''
+from argparse import ArgumentParser
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument('file', help='csv file to remove header of')
+    parser.add_argument(
+        '-o', '--output-path', help='output path of header less file'
+    )
+    return parser.parse_args()
 
 if __name__ == '__main__':
     main()
