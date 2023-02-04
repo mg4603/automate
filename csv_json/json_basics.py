7dc211834bf1287e3daf7af8952555c02416770e
Michael George <michael.g.george@gmail.com>
2023-02-04 08:03:23 +0530

    Add python_val_to_json to json_basics in csv_json

--- csv_json/json_basics.py
+++ csv_json/json_basics.py
@@ -4,6 +4,16 @@ def json_string_to_python_val(string_of_json):
     json_data_as_python_value = loads(string_of_json)
     print(json_data_as_python_value)
 
-json_string_to_python_val(
-    '{"name": "Zophie", "isCat": true, "miceCaught": 0,"felineIQ": null}'
-)
+def python_val_to_json(python_val):
+    return dumps(python_val)
+
+# print(
+    # type(python_val_to_json(
+        # {'isCat': True, 'miceCaught': 0, 'name': 'Zophie',
+    # 'felineIQ': None}
+    # ))
+# )
+
+# json_string_to_python_val(
+    # '{"name": "Zophie", "isCat": true, "miceCaught": 0,"felineIQ": null}'
+# )
