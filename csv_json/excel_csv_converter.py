c044e4610b37738bb5c7fa77de48cf5fde4d8acf
Michael George <michael.g.george@gmail.com>
2023-02-09 00:58:42 +0530

    Add runner code to excel_csv_converter in csv_json

---
+++ csv_json/excel_csv_converter.py
@@ -0,0 +1,9 @@
+'''
+1) take path as cmd line arg
+2) get all xlsx files in the dir
+3) transform all spread sheets into csv files - one per sheet
+4) name csv files <file_name>_<sheet_name>.csv
+'''
+
+if __name__ == '__main__':
+    main()
