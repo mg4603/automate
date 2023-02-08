f6d18f037bf23892beeea1bafd113dcb986a8f47
Michael George <michael.g.george@gmail.com>
2023-02-09 01:03:09 +0530

    Add parse_args to excel_csv_converter in csv_json

--- csv_json/excel_csv_converter.py
+++ csv_json/excel_csv_converter.py
@@ -4,6 +4,19 @@
 3) transform all spread sheets into csv files - one per sheet
 4) name csv files <file_name>_<sheet_name>.csv
 '''
+from argparse import ArgumentParser
+from sys import exit
+try:
+    from openpyxl import load_workbook
+except ImportError:
+    exit('This program requires the openpyxl module to run.')
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument('dir', help='path to dir of xlsx files to convert')
+    return parser.parse_args()
+
+
 
 if __name__ == '__main__':
     main()
