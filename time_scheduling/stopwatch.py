fb659b4a4db084e9c4813c2ccf5a9cbd954468eb
Michael George <michael.g.george@gmail.com>
2023-03-04 21:16:44 +0530

    Define StopWatch in time_scheduling

---
+++ time_scheduling/stopwatch.py
@@ -0,0 +1,9 @@
+'''
+1. track the amount of time between each key press
+2. print lap number, total time, lap time
+'''
+from time import time
+
+class StopWatch:
+    def __init__(self):
+        pass
