fc513d988c6be54208fdaf0c186b5934bc848a07
Michael George <michael.g.george@gmail.com>
2023-03-04 22:01:53 +0530

    Add stopwatch to StopWatch in time_scheduling

--- time_scheduling/stopwatch.py
+++ time_scheduling/stopwatch.py
@@ -14,7 +14,23 @@ class StopWatch:
         print('Afterward, press ENTER to "click" the stopwatch.')
         print('Press Ctrl-C to quit.')
         print()
-
+    
+    def stopwatch(self):
+        self.display_intro()
+        input()
+        print('Started')
+        laps = 1
+        start_time = time()
+        lap_start = start_time
+        while True:
+            input()
+            lap_end = time()
+            lap_time = round(lap_end - lap_start , 2)
+            total_time = round(lap_end - start_time, 2)
+            lap_start = lap_end
+            print('lap #%s %s(%s)' %(laps, total_time, lap_time), end='')
+            laps += 1
+            
 
 def main():
     stopwatch = StopWatch()
