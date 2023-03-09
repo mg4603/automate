cbb3578251f4d8611511389059cfce9c6399f03c
Michael George <michael.g.george@gmail.com>
2023-03-09 23:24:29 +0530

    Define TimeSheet in timesheet in time_scheduling

--- time_scheduling/timesheet.py
+++ time_scheduling/timesheet.py
@@ -1,3 +1,15 @@
+"""
+1. take a name as input
+2. check csv file for name
+3. if it exists:
+    1. if both check-in and check-out are filled keep looking
+    2. if only check in if filled in fill checkout
+    3. if non filled entry doesn't exist create new entry with 
+       check in time
+"""
+class TimeSheet:
+    def __init__(s, time_sheet_file):
+        s.time_sheet_file = time_sheet_file
 
 
 
