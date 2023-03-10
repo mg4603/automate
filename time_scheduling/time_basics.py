c58902d071153f4ac5c4c69a6bb5eecc08af717e
Michael George <michael.g.george@gmail.com>
2023-03-11 05:16:25 +0530

    Add datetime_obj to time_basics in time_scheduling

--- time_scheduling/time_basics.py
+++ time_scheduling/time_basics.py
@@ -21,6 +21,22 @@ def current_datetime():
         dt.day, dt.month, dt.year
     ))
 
+def datetime_obj(
+        year, month, day, hour = 0, minute = 0, second = 0
+):  
+    dt = datetime(year, month, day, hour, minute, second)
+    print('%s:%s:%s %s/%s/%s' % (
+        str(dt.hour).zfill(2), str(dt.minute).zfill(2),
+        str(dt.second).zfill(2), dt.day, dt.month, dt.year
+    ))
+
+
+
+# datetime_obj(2003, 3, 12)
+# datetime_obj(2003, 3, 12, 1)
+# datetime_obj(2003, 3, 12, 1, 3)
+# datetime_obj(2003, 4, 12, 1, 3, 2)
+
 # current_datetime()
 # rounding()
 # sleep_sample()
