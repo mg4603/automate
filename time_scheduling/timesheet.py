cfad71d0e7d6cb4b76e0629d9c2ba7192e4cdacb
Michael George <michael.g.george@gmail.com>
2023-03-10 05:34:21 +0530

    Add main to timesheet in time_scheduling

--- time_scheduling/timesheet.py
+++ time_scheduling/timesheet.py
@@ -7,11 +7,23 @@
     3. if non filled entry doesn't exist create new entry with 
        check in time
 """
+from sys import exit
+
 class TimeSheet:
     def __init__(s, time_sheet_file):
         s.time_sheet_file = time_sheet_file
 
-
+def main():
+    print('------------------------Time Sheet-----------------')
+    time_sheet = TimeSheet('timesheet.csv')
+    try:
+        while True:
+            print('Enter Name:')
+            name = input('> ')
+            time_sheet.record(name)
+            
+    except KeyboardInterrupt:
+        exit()
 
 if __name__ == '__main__':
     main()
