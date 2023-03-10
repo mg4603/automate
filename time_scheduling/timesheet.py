f6ee01593e9221a268ae1162d8b555d4c153ccd6
Michael George <michael.g.george@gmail.com>
2023-03-10 08:24:44 +0530

    Add record to TimeSheet in time_scheduling

--- time_scheduling/timesheet.py
+++ time_scheduling/timesheet.py
@@ -8,10 +8,44 @@
        check in time
 """
 from sys import exit
+from pathlib import Path
+from csv import reader, writer
+from time import time
+from logging import debug, disable, basicConfig, DEBUG, CRITICAL
+basicConfig(level=DEBUG, format='%(asctime)s - %(levelname)s - %(message)s ')
+disable(CRITICAL)
 
 class TimeSheet:
     def __init__(s, time_sheet_file):
-        s.time_sheet_file = time_sheet_file
+        s.time_sheet_file = Path(time_sheet_file)
+        if not s.time_sheet_file.exists():
+            debug(1)
+            s.time_sheet_file.open('w')
+    
+    def record(s, name):
+        time_sheet = []
+        with s.time_sheet_file.open('r') as file:
+            csv_reader = reader(file)
+            new_entry = True
+            for row in csv_reader:
+                if row[0] == name:
+                    debug(row)
+                    debug(row[0] == name)
+                    if not len(row) == 3:
+                        debug(len(row))
+                        new_entry = False
+                        time_sheet.append([row[0], row[1], time()])
+                    else:
+                        time_sheet.append(row)
+                else:
+                    time_sheet.append(row)
+            if new_entry:
+                time_sheet.append([name, time()])
+
+        with s.time_sheet_file.open('w') as file:
+            csv_writer = writer(file)
+            for row in time_sheet:
+                csv_writer.writerow(row)
 
 def main():
     print('------------------------Time Sheet-----------------')
