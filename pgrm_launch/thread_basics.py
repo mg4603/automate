f404b4bdf4a55feebfc7f03247ccabdfab495f6e
Michael George <michael.g.george@gmail.com>
2023-03-15 04:59:14 +0530

    Add thread_demo to thread_basics in pgrm_launch

--- pgrm_launch/thread_basics.py
+++ pgrm_launch/thread_basics.py
@@ -5,3 +5,10 @@ def take_nap(seconds):
     sleep(seconds)
     print('Napping thread wakes up!')
 
+def thread_demo():
+    print('Thread demo starts')
+    thread_obj = Thread(target=take_nap, args=[3])
+    thread_obj.start()
+    print('End of Thread demo')
+
+# thread_demo()
