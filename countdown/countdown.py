c45efaedea8e5c4f0d53cc9d230e250f2708afcd
Michael George <michael.g.george@gmail.com>
2023-03-26 23:30:44 +0530

    Add main to countdown in countdown

--- countdown/countdown.py
+++ countdown/countdown.py
@@ -1,8 +1,15 @@
 '''
-1. take a number(time in seconds)
+1. get countdown time
 2. count down to 0
 3. play a sound file when the countdown reaches zero
 '''
+
+def main():
+    print('Countdown Timer')
+    h, m, s = get_countdown_time()
+    countdown()
+    proc = Popen(split('see '))
+    proc.wait()
 
 if __name__ == '__main__':
     main()
