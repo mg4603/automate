d6c97791816c02f0d8c3a01025763b7b6d03fc35
Michael George <michael.g.george@gmail.com>
2023-03-26 23:55:53 +0530

    Add get_countdown_time to countdown in countdown

--- countdown/countdown.py
+++ countdown/countdown.py
@@ -3,11 +3,26 @@
 2. count down to 0
 3. play a sound file when the countdown reaches zero
 '''
+from subprocess import Popen
+from shlex import split
+from logging import debug, basicConfig, disable, DEBUG, CRITICAL
+basicConfig(level=DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+# disable(CRITICAL)
 
+def get_countdown_time():
+    print('Enter hours:')
+    prompt = '> '
+    h = get_num(prompt)
+    print('Enter minutes:')
+    m = get_num(prompt)
+    print('Enter seconds:')
+    s = get_num(prompt)
+    return h, m, s
+    
 def main():
     print('Countdown Timer')
     h, m, s = get_countdown_time()
-    countdown()
+    countdown(h, m, s)
     proc = Popen(split('see '))
     proc.wait()
 
